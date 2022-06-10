abcf2afbd26271c0e7ca5c6336df6669a1557bdb
Inamul <inamulhoque097@gmail.com>
2022-06-10 19:54:48 +0530

    Added: Daily Wage calculated.

--- src/EmpWageComputation.java
+++ src/EmpWageComputation.java
@@ -1,11 +1,14 @@
 public class EmpWageComputation {
     private static int isPresent = 1;
+    private static int empWagePerHr = 20;
     private static int isAbsent = 0;
+    private static int fullDayHour = 8;
     public static void main(String[] args) {
         System.out.println("Welcome to Employee wage computation program.");
         int check = (int) (Math.floor(Math.random()*10)%2);
         if (check == isPresent){
-            System.out.println("Employee is Present.");
+            int empWage = empWagePerHr*fullDayHour;
+            System.out.println("Employee is Present and daily wage is: "+empWage);
         } else {
             System.out.println("Employee is absent.");
         }
