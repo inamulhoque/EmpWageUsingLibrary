d2dc67a49d36fea1a8a75d2aebc90e4cc41ae771
Inamul <inamulhoque097@gmail.com>
2022-06-10 20:23:25 +0530

    Added: Switch Case

--- src/EmpWageComputation.java
+++ src/EmpWageComputation.java
@@ -1,20 +1,21 @@
 public class EmpWageComputation {
-    private static int isFullTime = 2;
-    private static int isPartTime = 1;
-    private static int isAbsent = 0;
-    private static int empWagePerHr = 20;
+    static int empWagePerHr = 20;
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee wage computation program.");
-        int check = (int) (Math.floor(Math.random() * 10) % 3);
+        int check = (int) (Math.floor(Math.random() * 10) % 3+1);
+        int isFullTime = 2;
+        int isPartTime = 1;
         int workHour = 0;
-        if (check == isFullTime) {
-            workHour = 8;
-            System.out.print("Employee is present Full time and ");
-        } else if (check == isPartTime) {
-            workHour = 4;
-            System.out.print("Employee is present Part time and ");
-        } else {
-            System.out.println("Employee is absent.");
+        switch (check) {
+            case 2:
+                workHour = 8;
+                System.out.print("Employee is present Full time and ");
+                break;
+            case 1:
+                workHour = 4;
+                System.out.print("Employee is present Part time and ");
+                break;
+            default:
+                System.out.println("Employee is absent.");
         }
         int empWage = empWagePerHr * workHour;
         System.out.println("daily wage is: " + empWage);
