151b09eaf3bb977802e39d7aef4f7fc11eee7ad1
Inamul <inamulhoque097@gmail.com>
2022-06-10 19:35:25 +0530

    Added: Employee is present or absent.

--- src/EmpWageComputation.java
+++ src/EmpWageComputation.java
@@ -1,5 +1,13 @@
 public class EmpWageComputation {
+    private static int isPresent = 1;
+    private static int isAbsent = 0;
     public static void main(String[] args) {
         System.out.println("Welcome to Employee wage computation program.");
+        int check = (int) (Math.floor(Math.random()*10)%2);
+        if (check == isPresent){
+            System.out.println("Employee is Present.");
+        } else {
+            System.out.println("Employee is absent.");
+        }
     }
 }
