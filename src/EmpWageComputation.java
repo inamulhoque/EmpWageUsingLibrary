70373933ca8b01b6579d4c595660097db1c8dba0
Inamul <inamulhoque097@gmail.com>
2022-06-10 22:05:13 +0530

    Added: Condition of maximum hour and maximum day

--- src/EmpWageComputation.java
+++ src/EmpWageComputation.java
@@ -1,24 +1,33 @@
 public class EmpWageComputation {
     static int empWagePerHr = 20;
     public static void main(String[] args) {
-        int check = (int) (Math.floor(Math.random() * 10) % 3+1);
         int isFullTime = 2;
         int isPartTime = 1;
-        int totalWorkDaysAMonth = 20;
         int workHour = 0;
-        switch (check) {
-            case 2:
-                workHour = 8;
-                System.out.print("Employee is present Full time and ");
-                break;
-            case 1:
-                workHour = 4;
-                System.out.print("Employee is present Part time and ");
-                break;
-            default:
-                System.out.println("Employee is absent.");
+        int days = 1;
+        int hours = 1;
+        int totalWorkHr = 0;
+        int empWage = 0;
+        while (days < 20 && totalWorkHr < 100) {
+            int check = (int) (Math.floor(Math.random() * 10) % 3 + 1);
+            switch (check) {
+                case 2:
+                    workHour = 8;
+                    System.out.println("Employee is present Full time and ");
+                    break;
+                case 1:
+                    workHour = 4;
+                    System.out.println("Employee is present Part time and ");
+                    break;
+                default:
+                    System.out.println("Employee is absent.");
+            }
+            days++;
+            totalWorkHr = totalWorkHr + workHour;
+            empWage = empWagePerHr * totalWorkHr;
         }
-        int empWage = empWagePerHr * workHour *totalWorkDaysAMonth;
+        System.out.println("Total Work hour: "+totalWorkHr);
+        System.out.println("Total days: "+days);
         System.out.println("daily wage is: " + empWage);
     }
 }
